96a696d4cb8fa3b3ddfe17c9e5912234437a86aa
Sidero,Cristian Martin <38406440+cmsider@users.noreply.github.com>
2020-05-18 17:21:20 -0300

    sad

--- CODIGO/AppParaNios/app/src/main/java/jyc/sa/ar/MainActivity.java
+++ CODIGO/AppParaNios/app/src/main/java/jyc/sa/ar/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imgVocal = (ImageView) findViewById(R.id.imageObj);
+        //imgVocal = (ImageView) findViewById(R.id.imageObj);
         generarImgRandom();
 
     }
